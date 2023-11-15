bfc728cfecef33f277d4f95fd92376a0f1f58bcd
Segun <lawalsegun72@gmail.com>
2023-11-15 20:05:39 +0100

    pycodestyle fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,9 +4,10 @@
 This contains a 2D matrix rotation function
 """
 
+
 def rotate_2d_matrix(matrix):
     """ Function rotates a 2D matrix 90 degrees clockwise"""
-    
+
     n = len(matrix)
 
     for i in range(n):

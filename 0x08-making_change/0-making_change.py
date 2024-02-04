cf4e6c733fb07b01b6fdef665561c5f89b7fcf88
Segun <lawalsegun72@gmail.com>
2024-02-04 01:11:11 +0100

    pep8 fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,7 +9,7 @@ def makeChange(coins, total):
     """ Function determines the fewest number of coins needed to meet a given amount total"""
     if total <= 0:
         return 0
-    
+
     minCoins = [0] + [total + 1] * total
 
     for amount in range(1, total + 1):
